ceacd2a07d5660a20992d21f113d58d2af133622
VyomOp2 <vyom.malo6904@gmail.com>
2024-04-12 10:40:11 +0530

    Added User routes to User-Controller

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,18 @@
 import { Router } from "express";
-import { loginUser, logoutUser, registerUser , refreshAccessToken } from "../controllers/user.controller.js"
+import { 
+    loginUser,
+    logoutUser,
+    registerUser , 
+    refreshAccessToken, 
+    changeCurrentPassword, 
+    getCurrentUser, 
+    updateAccountDetails, 
+    updateUserAvatar, 
+    updateUserCoverImage, 
+    getUserChannelProfile, 
+    getWatchHistory 
+}
+from "../controllers/user.controller.js"
 import { upload } from "../middlewares/multer.middleware.js"
 import { verifyJWT } from "../middlewares/authentication.middleware.js"
 
@@ -24,4 +37,14 @@ router.route("/login").post(loginUser)
 // Secured Routes :
 router.route("/logout").post( verifyJWT , logoutUser )
 router.route("/refresh-token").post(refreshAccessToken)
+router.route("/change-password").post(verifyJWT , changeCurrentPassword)
+router.route("/current-user").get(verifyJWT , getCurrentUser)
+router.route("/update-account").patch(verifyJWT , updateAccountDetails)
+router.route("/avatar-update").patch(verifyJWT , upload.single("avatar") , updateUserAvatar)
+router.route("/cover-image-update").patch(verifyJWT , upload.single("coverImage") , updateUserCoverImage)
+
+// User Channel Routes :
+router.route("/channel/:username").get(verifyJWT , getUserChannelProfile)
+router.route("/history").get(verifyJWT , getWatchHistory)
+
 export default router
